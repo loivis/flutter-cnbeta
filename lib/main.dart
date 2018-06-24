9ba502146ee3258f1e941a7571b821ffde66c4a1
loivis <github@kinase.wang>
2018-06-24 21:39:39 +0200

    fetch timeline.json

--- lib/main.dart
+++ lib/main.dart
@@ -1,65 +1,65 @@
-// import 'dart:async';
-// import 'dart:convert';
-// import 'package:flutter/material.dart';
-// import 'package:http/http.dart' as http;
-// import 'package:cnbeta/news_item.dart';
+import 'dart:async';
+import 'dart:convert';
+import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
+import 'package:cnbeta/news_item.dart';
 
-// void main() => runApp(new MyApp());
+void main() => runApp(new MyApp());
 
-// class MyApp extends StatelessWidget {
-//   // This widget is the root of your application.
-//   @override
-//   Widget build(BuildContext context) {
-//     return new MaterialApp(
-//       title: 'cnBeta News',
-//       theme: new ThemeData(
-//         primarySwatch: Colors.blue,
-//       ),
-//       home: new Scaffold(
-//         appBar: new AppBar(
-//           title: new Text('cnBeta 资讯'),
-//         ),
-//         body: FutureBuilder<NewsItem>(
-//           future: fetchNews(),
-//           builder: (context, snapshot) {
-//             if (snapshot.hasData) {
-//               // return Text(snapshot.data.title);
-//               return new ListView(
-//                 children: <Widget>[
-//                   new ListTile(
-//                     title: new Text(snapshot.data.title),
-//                     subtitle: new Text(snapshot.data.inputtime),
-//                     trailing: Image.network(snapshot.data.thumb),
-//                   ),
-//                 ],
-//               );
-//             } else if (snapshot.hasError) {
-//               return Text("${snapshot.error}");
-//             }
-//             return CircularProgressIndicator();
-//           },
-//         ),
-//       ),
-//     );
-//   }
-// }
+class MyApp extends StatelessWidget {
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return new MaterialApp(
+      title: 'cnBeta News',
+      theme: new ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: new Scaffold(
+        appBar: new AppBar(
+          title: new Text('cnBeta 资讯'),
+        ),
+        body: FutureBuilder<NewsItem>(
+          future: fetchNews(),
+          builder: (context, snapshot) {
+            if (snapshot.hasData) {
+              // return Text(snapshot.data.title);
+              return new ListView(
+                children: <Widget>[
+                  new ListTile(
+                    title: new Text(snapshot.data.title),
+                    subtitle: new Text(snapshot.data.inputtime),
+                    trailing: Image.network(snapshot.data.thumb),
+                  ),
+                ],
+              );
+            } else if (snapshot.hasError) {
+              return Text("${snapshot.error}");
+            }
+            return CircularProgressIndicator();
+          },
+        ),
+      ),
+    );
+  }
+}
 
-// class Content extends StatelessWidget {
-//   @override
-//   Widget build(BuildContext context) {
-//     return new Text('news');
-//   }
-// }
+class Content extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return new Text('news');
+  }
+}
 
-// Future<NewsItem> fetchNews() async {
-//   final response =
-//       await http.get('https://m.cnbeta.com/touch/default/timeline.json');
+Future<NewsItem> fetchNews() async {
+  final response =
+      await http.get('https://m.cnbeta.com/touch/default/timeline.json');
 
-//   if (response.statusCode == 200) {
-//     // If server returns an OK response, parse the JSON
-//     return NewsItem.fromJson(json.decode(response.body)['result']['list'][0]);
-//   } else {
-//     // If that response was not OK, throw an error.
-//     throw Exception('Failed to load news');
-//   }
-// }
+  if (response.statusCode == 200) {
+    // If server returns an OK response, parse the JSON
+    return NewsItem.fromJson(json.decode(response.body)['result']['list'][0]);
+  } else {
+    // If that response was not OK, throw an error.
+    throw Exception('Failed to load news');
+  }
+}
--- lib/news_item.dart
+++ lib/news_item.dart
@@ -1,94 +1,94 @@
-// class NewsItem {
-//   final String aid;
-//   final String bad;
-//   final String catid;
-//   final String collectnum;
-//   final String comments;
-//   final String counter;
-//   final String good;
-//   final String inputtime;
-//   final String keywords;
-//   final String label;
-//   final String mview;
-//   final String pollid;
-//   final String premium;
-//   final String queueid;
-//   final String ratings;
-//   final String ratingsStory;
-//   final String score;
-//   final String scoreStory;
-//   final String sid;
-//   final String source;
-//   final String sourceid;
-//   final String thumb;
-//   final String title;
-//   final String topic;
-//   final String updatetime;
-//   final String url;
-//   final String urlShow;
-//   final String userId;
+class NewsItem {
+  final String aid;
+  final String bad;
+  final String catid;
+  final String collectnum;
+  final String comments;
+  final String counter;
+  final String good;
+  final String inputtime;
+  final String keywords;
+  final String label;
+  final String mview;
+  final String pollid;
+  final String premium;
+  final String queueid;
+  final String ratings;
+  final String ratingsStory;
+  final String score;
+  final String scoreStory;
+  final String sid;
+  final String source;
+  final String sourceid;
+  final String thumb;
+  final String title;
+  final String topic;
+  final String updatetime;
+  final String url;
+  final String urlShow;
+  final String userId;
 
-//   NewsItem({
-//     this.aid,
-//     this.bad,
-//     this.catid,
-//     this.collectnum,
-//     this.comments,
-//     this.counter,
-//     this.good,
-//     this.inputtime,
-//     this.keywords,
-//     this.label,
-//     this.mview,
-//     this.pollid,
-//     this.premium,
-//     this.queueid,
-//     this.ratings,
-//     this.ratingsStory,
-//     this.score,
-//     this.scoreStory,
-//     this.sid,
-//     this.source,
-//     this.sourceid,
-//     this.thumb,
-//     this.title,
-//     this.topic,
-//     this.updatetime,
-//     this.url,
-//     this.urlShow,
-//     this.userId,
-//   });
+  NewsItem({
+    this.aid,
+    this.bad,
+    this.catid,
+    this.collectnum,
+    this.comments,
+    this.counter,
+    this.good,
+    this.inputtime,
+    this.keywords,
+    this.label,
+    this.mview,
+    this.pollid,
+    this.premium,
+    this.queueid,
+    this.ratings,
+    this.ratingsStory,
+    this.score,
+    this.scoreStory,
+    this.sid,
+    this.source,
+    this.sourceid,
+    this.thumb,
+    this.title,
+    this.topic,
+    this.updatetime,
+    this.url,
+    this.urlShow,
+    this.userId,
+  });
 
-//   factory NewsItem.fromJson(Map<String, dynamic> json) {
-//     return NewsItem(
-//       aid: json['aid'],
-//       bad: json['bad'],
-//       catid: json['catid'],
-//       collectnum: json['collectnum'],
-//       comments: json['comments'],
-//       counter: json['counter'],
-//       good: json['good'],
-//       inputtime: json['inputtime'],
-//       keywords: json['keywords'],
-//       label: json['label'],
-//       mview: json['mview'],
-//       pollid: json['pollid'],
-//       premium: json['premium'],
-//       queueid: json['queueid'],
-//       ratings: json['ratings'],
-//       ratingsStory: json['ratingsStory'],
-//       score: json['score'],
-//       scoreStory: json['scoreStory'],
-//       sid: json['sid'],
-//       source: json['source'],
-//       sourceid: json['sourceid'],
-//       thumb: json['thumb'],
-//       title: json['title'],
-//       topic: json['topic'],
-//       updatetime: json['updatetime'],
-//       url: json['url'],
-//       urlShow: json['urlShow'],
-//       userId: json['userId'],
-//     );
-//   }
-// }
+  factory NewsItem.fromJson(Map<String, dynamic> json) {
+    return NewsItem(
+      aid: json['aid'],
+      bad: json['bad'],
+      catid: json['catid'],
+      collectnum: json['collectnum'],
+      comments: json['comments'],
+      counter: json['counter'],
+      good: json['good'],
+      inputtime: json['inputtime'],
+      keywords: json['keywords'],
+      label: json['label'],
+      mview: json['mview'],
+      pollid: json['pollid'],
+      premium: json['premium'],
+      queueid: json['queueid'],
+      ratings: json['ratings'],
+      ratingsStory: json['ratingsStory'],
+      score: json['score'],
+      scoreStory: json['scoreStory'],
+      sid: json['sid'],
+      source: json['source'],
+      sourceid: json['sourceid'],
+      thumb: json['thumb'],
+      title: json['title'],
+      topic: json['topic'],
+      updatetime: json['updatetime'],
+      url: json['url'],
+      urlShow: json['urlShow'],
+      userId: json['userId'],
+    );
+  }
+}

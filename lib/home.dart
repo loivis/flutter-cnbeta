e1301ecad81c604fec2252e6e4dce404aae7323b
loivis <github@kinase.wang>
2018-07-13 10:34:22 +0200

    extract itemBuilder from ListView.builder

--- lib/home.dart
+++ lib/home.dart
@@ -45,6 +45,30 @@ class _HomeState extends State<Home> {
     );
   }
 
+  Widget _buildBody(BuildContext context) {
+    var content;
+
+    if (_newsList.isEmpty) {
+      content = new Center(
+        child: new CircularProgressIndicator(),
+      );
+    } else {
+      content = ListView.builder(
+        physics: AlwaysScrollableScrollPhysics(),
+        controller: _scrollController,
+        itemCount: _newsList.length,
+        itemBuilder: _buildListViewItem,
+      );
+    }
+
+    var _refreshIndicator = new RefreshIndicator(
+      onRefresh: _loadLatest,
+      child: content,
+    );
+
+    return _refreshIndicator;
+  }
+
   FloatingActionButton _buildFloatingActionButton() {
     return new FloatingActionButton(
       child: _updateInProgress
@@ -73,64 +97,42 @@ class _HomeState extends State<Home> {
     );
   }
 
-  Widget _buildBody(BuildContext context) {
-    var content;
-
-    if (_newsList.isEmpty) {
-      content = new Center(
-        child: new CircularProgressIndicator(),
-      );
-    } else {
-      content = ListView.builder(
-        physics: AlwaysScrollableScrollPhysics(),
-        controller: _scrollController,
-        itemCount: _newsList.length,
-        itemBuilder: (context, i) {
-          // print('builder index: $i');
-          if (i.isOdd) {
-            return new Divider();
-          }
-          final index = i ~/ 2;
-          var news = _newsList[index];
-          return new ListTile(
-            title: news.title.startsWith('<')
-                ? new Text(
-                    news.title.replaceAll(new RegExp('</span>|<span.*">'), ''),
-                    style: TextStyle(
-                        color: Colors.red, fontWeight: FontWeight.bold))
-                : new Text(news.title),
-            subtitle: new Text(news.label + ' | ' + news.inputtime),
-            trailing: new Container(
-              width: 50.0,
-              height: 50.0,
-              decoration: new BoxDecoration(
-                shape: BoxShape.circle,
-                border: new Border.all(color: Colors.grey, width: 2.0),
-                image: new DecorationImage(
-                  image: new NetworkImage(news.thumb),
-                  // image: new CachedNetworkImageProvider(news.thumb),
-                ),
-              ),
-            ),
-            onTap: () {
-              Navigator.push(
-                context,
-                new MaterialPageRoute(
-                  builder: (context) => new NewsView(news),
-                ),
-              );
-            },
-          );
-        },
-      );
+  Widget _buildListViewItem(BuildContext context, int i) {
+    // print('builder index: $i');
+    if (i.isOdd) {
+      return new Divider();
     }
-
-    var _refreshIndicator = new RefreshIndicator(
-      onRefresh: _loadLatest,
-      child: content,
+    final index = i ~/ 2;
+    var news = _newsList[index];
+    return new ListTile(
+      title: news.title.startsWith('<')
+          ? new Text(
+              news.title.replaceAll(new RegExp('</span>|<span.*">'), ''),
+              style: TextStyle(color: Colors.red, fontWeight: FontWeight.bold),
+            )
+          : new Text(news.title),
+      subtitle: new Text(news.label + ' | ' + news.inputtime),
+      trailing: new Container(
+        width: 50.0,
+        height: 50.0,
+        decoration: new BoxDecoration(
+          shape: BoxShape.circle,
+          border: new Border.all(color: Colors.grey, width: 2.0),
+          image: new DecorationImage(
+            image: new NetworkImage(news.thumb),
+            // image: new CachedNetworkImageProvider(news.thumb),
+          ),
+        ),
+      ),
+      onTap: () {
+        Navigator.push(
+          context,
+          new MaterialPageRoute(
+            builder: (context) => new NewsView(news),
+          ),
+        );
+      },
     );
-
-    return _refreshIndicator;
   }
 
   Future<List<NewsInfo>> _getNewsList() async {
